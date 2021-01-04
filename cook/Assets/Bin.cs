cf82f174327506e0f27f3c0e792de22b374c67b7
dohaisalive <dohaisalive@gmail.com>
2021-01-04 15:46:41 +0300

    slots + cuttingboard functionality

---
+++ cook/Assets/Bin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bin : MonoBehaviour
+{
+
+    [SerializeField] public Transform bottombun;
+    [SerializeField] public Transform topbun;
+    [SerializeField] public Transform hotdog;
+    [SerializeField] public Transform burger;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "bun")
+        {
+            if (GamePlay.cuttingboard1 == "empty")
+            {
+                Instantiate(bottombun, new Vector2(-1.27f, -2.28f), bottombun.rotation);
+                Instantiate(topbun, new Vector2(-1.324f, -2.175f), topbun.rotation);
+                GamePlay.cuttingboard1 = "full";
+            }
+
+            else if (GamePlay.cuttingboard2 == "empty")
+            {
+                Instantiate(bottombun, new Vector2(0.34f, -2.28f), bottombun.rotation);
+                Instantiate(topbun, new Vector2(0.28f, -2.14f), topbun.rotation);
+                GamePlay.cuttingboard2 = "full";
+            }
+
+            else if (GamePlay.cuttingboard3 == "empty")
+            {
+                Instantiate(bottombun, new Vector2(1.98f, -2.27f), bottombun.rotation);
+                Instantiate(topbun, new Vector2(1.90f, -2.09f), topbun.rotation);
+                GamePlay.cuttingboard3 = "full";
+            }
+
+        }
+
+      
+    }
+}
---
+++ cook/Assets/GamePlay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static string cuttingboard1 = "empty";
+    public static string cuttingboard2 = "empty";
+    public static string cuttingboard3 = "empty";
+
+    public static string grill1;
+    public static string grill2; 
+    public static string grill3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
